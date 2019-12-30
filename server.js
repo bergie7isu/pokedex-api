bac73c9da52507c6f73a774584d0a767cb99ce6f
Mark Bergstrom <markbergstrom@gmail.com>
2019-12-30 12:17:19 -0600

    authorization check implemented

--- server.js
+++ server.js
@@ -1,13 +1,54 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const POKEDEX = require('./pokedex.json')
 
 const app = express();
 
 app.use(morgan('dev'));
 
-app.use((req, res) => {
-    res.send('Hello, world!');
+const validTypes = [
+    `Bug`,
+    `Dark`,
+    `Dragon`,
+    `Electric`,
+    `Fairy`,
+    `Fighting`,
+    `Fire`,
+    `Flying`,
+    `Ghost`,
+    `Grass`,
+    `Ground`,
+    `Ice`,
+    `Normal`,
+    `Poison`,
+    `Psychic`,
+    `Rock`,
+    `Steel`,
+    `Water`
+];
+
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN;
+    const authToken = req.get('Authorization');
+    console.log('validate bearer token middleware');
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+        return res.status(401).json({ error: 'Unauthorized request!' });
+    }
+    next();
 });
+
+function handleGetTypes(req, res) {
+    res.json(validTypes);
+};
+
+function handleGetPokemon(req, res) {
+    res.send('Hello, Pokemon!');
+}
+
+app.get('/types', handleGetTypes);
+
+app.get('/pokemon', handleGetPokemon);
 
 const PORT = 8000;
 
